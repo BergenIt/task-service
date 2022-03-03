778038675b302940a223a586e475f6b1d8f6d93c
Юрий Турбин <YVTURBIN@BG.loc>
2022-03-03 23:48:43 +0300

    relation .moduler

--- GenerateSwagger.ps1
+++
@@ -1,5 +0,0 @@
-dotnet build task-service.sln
-cp .\TaskService.Main\ProtobufClient.xml ProtobufClient.xml
-dotnet swagger tofile --yaml --output doc/swagger.yaml .\TaskService.Main\bin\Debug\net6.0\TaskService.dll v1
-rm .\ProtobufClient.xml
-
